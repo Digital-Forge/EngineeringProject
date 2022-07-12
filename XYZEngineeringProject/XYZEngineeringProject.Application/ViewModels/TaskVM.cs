6b256de551d89dc7f0530b8216feb6f1dd644d88
IgnacyPawlak <56115888+IgnacyPawlak@users.noreply.github.com>
2022-07-12 21:16:26 +0200

    taskVM and taskview

---
+++ XYZEngineeringProject/XYZEngineeringProject.Application/ViewModels/TaskVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XYZEngineeringProject.Domain.Models;
+
+namespace XYZEngineeringProject.Application.ViewModels
+{
+    public class TaskVM
+    {
+        public Guid Id { get; set; }
+        public DateTime Deadline { get; set; }
+        public Priority Priority { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public Guid? ListOfTasksId { get; set; }
+        public ListOfTasks? ListOfTasks { get; set; }
+    }
+}
---
+++ XYZEngineeringProject/XYZEngineeringProject.Web/Controllers/TaskController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace XYZEngineeringProject.Web.Controllers
+{
+    public class TaskController: Controller
+    {
+        private readonly ILogger<TaskController> _logger;
+
+        public TaskController(ILogger<TaskController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

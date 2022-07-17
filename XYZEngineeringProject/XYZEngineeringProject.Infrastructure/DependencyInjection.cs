9284641e37421f11340c0076bb24bf600db1513b
Digital-Forge <jan.m.szkudlarek@gmail.com>
2022-07-17 14:26:24 +0200

    Add base empty structure of Repository

--- XYZEngineeringProject/XYZEngineeringProject.Infrastructure/DependencyInjection.cs
+++ XYZEngineeringProject/XYZEngineeringProject.Infrastructure/DependencyInjection.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using XYZEngineeringProject.Domain.Interfaces;
+using XYZEngineeringProject.Infrastructure.Repositories;
 
 namespace XYZEngineeringProject.Infrastructure
 {
@@ -11,6 +13,12 @@ namespace XYZEngineeringProject.Infrastructure
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services)
         {
+            services.AddTransient<IUserRepository, UserRepository>();
+            services.AddTransient<IClientRepository, ClientRepository>();
+            services.AddTransient<IDepartmentRepository, DepartmentRepository>();
+            services.AddTransient<INoteRepository, NoteRepository>();
+            services.AddTransient<IPositionRepository, PositionRepository>();
+            services.AddTransient<ITaskRepository, TaskRepository>();
             //services.AddTransient<I, >();
             
             return services;
---
+++ XYZEngineeringProject/XYZEngineeringProject.Infrastructure/Repositories/TaskRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XYZEngineeringProject.Domain.Interfaces;
+
+namespace XYZEngineeringProject.Infrastructure.Repositories
+{
+    public class TaskRepository : ITaskRepository
+    {
+    }
+}

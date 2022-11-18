b4c5cef733034d973acc876397414d6fa75634f9
Digital-Forge <>
2022-11-18 19:26:40 +0100

    roboczy

---
+++ XYZEngineeringProject/XYZEngineeringProject.Application/Interfaces/IFileService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XYZEngineeringProject.Application.ViewModels.File;
+
+namespace XYZEngineeringProject.Application.Interfaces
+{
+    public interface IFileService
+    {
+        FileStructureVM GetStructure();
+    }
+}
---
+++ XYZEngineeringProject/XYZEngineeringProject.Domain/Models/File/Directory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XYZEngineeringProject.Domain.Models.File
+{
+    public class Directory
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public string PathMask { get; set; }
+        public int DeepLevel { get; set; }
+
+        // relation
+        public Guid ParentDirectoryId { get; set; }
+        public virtual Directory ParentDirectory { get; set; }
+        public virtual List<Directory> ChildDirectories { get; set; }
+        public virtual List<File> Files { get; set; }
+        public virtual List<AccessDirectory> Departments { get; set; }
+
+    }
+}

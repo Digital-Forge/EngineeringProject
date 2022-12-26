8cd5fd5069093ffbf959992af9941e790593fd38
IgnacyPawlak <56115888+IgnacyPawlak@users.noreply.github.com>
2022-12-26 12:33:57 +0100

    cofnięcie zmiany
    cofnięcie zmiany

--- XYZEngineeringProject/XYZEngineeringProject.Application/Services/AuthorizationService.cs
+++ XYZEngineeringProject/XYZEngineeringProject.Application/Services/AuthorizationService.cs
@@ -60,7 +60,7 @@ namespace XYZEngineeringProject.Application.Services
             var roles = _context.UserRoles.Where(x => x.UserId == user.Id).ToList();
             foreach (var role in roles)
             {
-                claims.Add(new Claim("Role", _context.Roles.First(x => x.Id == role.RoleId).Name));
+                claims.Add(new Claim(ClaimTypes.Role, _context.Roles.First(x => x.Id == role.RoleId).Name));
             }         
 
             var token = new JwtSecurityToken(_config["JWT:Issuer"], _config["JWT:Issuer"], claims, null, DateTime.Now.AddMinutes(120), credentials);

ea43cfb0e65f919e23c78642efeadc4d1837d56d
IgnacyPawlak <56115888+IgnacyPawlak@users.noreply.github.com>
2023-01-04 22:19:50 +0100

    fix

--- XYZEngineeringProject/XYZEngineeringProject.Web/Controllers/AuthorizationController.cs
+++ XYZEngineeringProject/XYZEngineeringProject.Web/Controllers/AuthorizationController.cs
@@ -53,7 +53,7 @@ namespace XYZEngineeringProject.Web.Controllers
         [HttpGet]
         public IActionResult CheckNick(string name)
         {
-            return Ok()
+            return Ok();
         }
     }
 }

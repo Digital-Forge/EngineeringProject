1a61fd634357b2d877404a798fbcb2ff4d529418
Digital-Forge <>
2023-01-06 17:42:17 +0100

    Dodano automatyczne wgrywanie migracji

--- XYZEngineeringProject/XYZEngineeringProject.Web/Program.cs
+++ XYZEngineeringProject/XYZEngineeringProject.Web/Program.cs
@@ -72,6 +72,22 @@ if (app.Environment.IsDevelopment())
 }
 else
 {
+    using (var scope = app.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+
+        try
+        {
+            var context = services.GetRequiredService<Context>();
+            context.Database.Migrate(); // apply all migrations
+        }
+        catch (Exception ex)
+        {
+            var logger = services.GetRequiredService<ILogger<Program>>();
+            logger.LogError(ex, "An error occurred seeding the DB.");
+        }
+    }
+
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
